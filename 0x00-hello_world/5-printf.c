61779a100619aa7359f34277c907540344f93bde
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-15 14:49:27 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - print text
+ * Return:0
+ */
+int main(void) {
+    printf("exactly with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
