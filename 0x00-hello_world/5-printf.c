0d7906331e11401500634f8efe3cd12a02def1f1
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-15 14:55:18 -0700

    5-code fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - print text
  * Return:0
  */
-int main(void) {
-    printf("exactly with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
