2844e2b6a013640c28dfec47c5d8ddd678e8d2f5
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-17 12:56:36 -0700

    from z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - print lower case z to a
+  * Return: 0
+  */
+
+int main(void)
+{
+char x;
+
+for (x = 'z' ; x <= 'a' ; x--)
+{
+putchar('x');
+}
+
+putchar('\n');
+
+return (0);
+}
