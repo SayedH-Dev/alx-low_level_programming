3ef5f3668c34da730c2ee4ee842889ff08b4dcec
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-17 06:39:53 -0700

    lower then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - print lower then uppercase
+  * Return: 0
+  */
+
+int main(void)
+{
+char b;
+
+while (b <= 'z')
+}
+putchar(b);
+b++
+}
+while (b <= 'Z')
+}
+putchar(b);
+b++
+}
+
+putchar('\n')
+
+return (0);
+}
