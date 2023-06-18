049b707e6debe15a0271dd304056356c617792e6
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-18 02:05:59 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - print hexadecimal
+  * Return: 0
+  */
+
+int main(void)
+{
+char x;
+
+for (x = 0 ; x <= 9 ; x++)
+{
+putchar(x);
+}
+
+for (x = 'a' ; x <= 'f' ; x++)
+{
+putchar(x);
+}
+
+putchar('\n');
+
+return (0);
+}
