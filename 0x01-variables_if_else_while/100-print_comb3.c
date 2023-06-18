12ef210c9e479b7b0cf09c31a8bdaaec3efe80c3
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-18 02:55:27 -0700

    possible combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - possible combination of 2 digits
+  * Return: 0
+  */
+
+int main(void)
+{
+int x;
+int y;
+
+for (x = '0' ; x <= '8' ; x++)
+{
+for (y = 'x + 1' ; y <= '9' ; y++)
+{
+putchar(xy);
+if (x != '8' && y != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
