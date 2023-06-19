ea104bd351399488eaa23e9d2078b84930abea58
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-19 06:42:41 -0700

    -code fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,26 @@
 #include <stdio.h>
 
 /**
-  * main - possible combination of 2 double digits
-  * Return: 0
-  */
-
+ * main - possible combination of 2 double digits
+ * Return: 0
+ */
 int main(void)
 {
 int x;
 int y;
-int z;
-int t;
 
-for (x = '0' ; x <= '9' ; x++)
+for (x = 0; x <= 98; x++)
 {
-for (y = '0' ; y <= '9' ; y++)
+for (y = x + 1; y <= 99; y++)
 {
-if (y == x)
-{
-continue;
-}
-for (z = '0' ; z <= '9' ; z++)
-{
-if (z == x && z == y)
-{
-continue;
-}
-for (t = '1' ; t <= '9' ; t++)
-{
-if (t == x && t == y && t == z)
-{
-continue;
-}
-putchar(x);
-putchar(y);
+putchar('0' + x / 10);
+putchar('0' + x % 10);
 putchar(' ');
-putchar(z);
-putchar(t);
-if (x != '9' && y != '8' && z != '7' && t != '6')
+putchar('0' + y / 10);
+putchar('0' + y % 10);
+if (x != 98)
+{
+if (y != 99)
 {
 putchar(',');
 putchar(' ');
@@ -45,7 +28,8 @@ putchar(' ');
 }
 }
 }
-}
+
 putchar('\n');
+
 return (0);
 }
