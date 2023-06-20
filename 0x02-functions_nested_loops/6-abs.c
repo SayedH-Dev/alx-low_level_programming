5adcdabfd22838320e8676a37b16ad5b7a14ed25
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-20 15:44:45 -0700

    absolute no.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _abs - computes the absolut value
+  * @int: number
+  * Return: 0
+  */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+
+	return (0);
+}
