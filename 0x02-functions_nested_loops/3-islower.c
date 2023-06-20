e6b5bf3d962eff8e90f8d8d67ff5d97a66c83821
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-20 13:46:46 -0700

    checking lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _islower - checking lower case
+  * Return: 1 if lowercase 0 if not
+  */
+
+int _islower(int c)
+{
+	if (c >= 'a')
+	{
+		if (c <= 'z')
+		{
+			return (1);
+			else
+				return (0);
+		}
+	}
+
+}
