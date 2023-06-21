36713bd9869c55ec4b0fb2a2215674f66fd08cf7
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-21 14:48:40 -0700

    count to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * print_to_98 - print from number n to 98
+  * @n: counting start number
+  * Return: void
+  */
+void print_to_98(int n)
+{
+	int x;
+
+	if (n <= 98)
+	{
+		for (x = n ; x <= 98 ; x++)
+		{
+			printf("%d", x);
+			if (x != 98)
+			{
+				printf(", ");
+			}
+		}
+		else
+		{
+			for (x = n ; x > 98 ; x--)
+			{
+				printf("%d", x);
+				if (x != 98)
+				{
+					printf(", ");
+				}
+			}
+		}
+
+		printf("\n");
+	}
+}
