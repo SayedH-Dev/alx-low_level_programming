ece7b7fb13c845a340ed3b7d191f4eb9bb9d60cc
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-22 08:47:14 -0700

    trial

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,30 @@
 #include <stdio.h>
 
 /**
-  * main - testing zero
-  * Return= 0
+  * positive_or_negative - checking number sign
+  * @i: number to be checked
+  * Return: void
   */
-int main(void) 
+void positive_or_negative(int i)
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+/**
+  * main - testing zero
+  * Return: 0
+  */
+int main(void)
 {
 
 	int i = 0;
-	
+
 	printf("testing zero= ");
 	positive_or_negative(i);
-	
-	return 0;
+
+	return (0);
 }
