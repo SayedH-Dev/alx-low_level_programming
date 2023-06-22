1f78142049d8b2a3b5f2fba208d24ad43929a1b5
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-22 09:07:39 -0700

    wtf

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,42 +4,17 @@
 #include <time.h>
 
 /**
-  * positive_or_negative - checking number + - 0
-  * @i: number to be checked
-  * Return: void
-  */
-void positive_or_negative(int i)
-{
-int i;
-
-srand(time(0));
-i = rand() - RAND_MAX / 2;
-
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i < 0)
-{
-printf("%d is negative\n", i);
-}
-else
-{
-printf("%d is zero\n", i);
-}
-}
-
-/**
-  * main - testing function
+  * main - testing sign
   * Return: 0
   */
 int main(void)
 {
+	int n;
 
-	int i = 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-	printf("testing zero= ");
-	positive_or_negative(i);
+	positive_or_negative(n);
 
 	return (0);
 }
