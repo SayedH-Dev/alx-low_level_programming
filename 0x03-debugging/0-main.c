59c0dcb9fea0eb394dd8e14924310905a880b11b
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-22 08:55:48 -0700

    trial

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,36 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
-  * positive_or_negative - checking number sign
+  * positive_or_negative - checking number + - 0
   * @i: number to be checked
   * Return: void
   */
 void positive_or_negative(int i)
 {
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
+int i;
 
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else
+{
+printf("%d is zero\n", i);
+}
+}
+
 /**
-  * main - testing zero
+  * main - testing function
   * Return: 0
   */
 int main(void)
