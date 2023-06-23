1e179ab83b0a749928134bafcdce7c23f7ebabee
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-23 13:58:43 -0700

    print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_line - print a straight line
+  * @n: check how long the line is
+  * Return: void
+  */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	int x;
+
+	for (x = 0 ; x < n ; x++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
