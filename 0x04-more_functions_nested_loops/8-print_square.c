ae4b20e5ec5f5bd6d74392602538735e6decca1a
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-23 14:44:00 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,14 +13,14 @@ void print_square(int size)
 	{
 		int x;
 		int y;
-		
+
 		for (x = 0 ; x < size ; x++)
 		{
 			for (y = 0 ; y < size ; y++)
 			{
 				_putchar('#');
 			}
-			
+
 			_putchar('\n');
 		}
 	}
