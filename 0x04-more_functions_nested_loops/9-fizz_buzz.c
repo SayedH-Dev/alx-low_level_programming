0a20ed90af194f92bf1398c7ef61d9ac4748998e
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-23 15:20:21 -0700

    fizzbuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,7 @@ int main(void)
 
 	for (x = 1 ; x <= 100 ; x++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (x % 3 == 0 && x % 5 != 0)
+		if (x % 3 == 0 && x % 5 != 0)
 		{
 			printf(" Fizz");
 		}
@@ -22,6 +18,10 @@ int main(void)
 		{
 			printf(" Buzz");
 		}
+		else if (x % 3 == 0 && x % 5 == 0)
+                {
+                        printf(" FizzBuzz");
+                }
 		else if (x == 1)
 		{
 			printf("%d", x);
