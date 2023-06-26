5fe68b3efd2478296b346f2189c4598b1d69c924
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-26 13:25:30 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <sdtio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  * puts2 - function that print every other char in a string
+  * @str: string
+  * Return: void
+  */
+void puts2(char *str)
+{
+	int x;
+
+	if (str == NULL)
+	{
+		return;
+	}
+
+	x = 0;
+	while (str[x])
+	{
+		printf("%c", str[x]);
+		x = x + 2;
+	}
+	printf("\n");
+}
