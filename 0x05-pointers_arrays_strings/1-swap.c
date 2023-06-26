fe9e7ec5d8436e1bfc50e0c05df7681ada88c3a2
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-06-26 08:45:15 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * swap_int - function to swap value of 2 int
+  * @a: pointer to int
+  * @b: pointer to int
+  * Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	int store;
+
+	store = *a;
+	a* = b*;
+	*b = store;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 /*functions declaration*/
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _putchar(char x);
 
 #endif
