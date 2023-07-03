e10be20cfe76815bb8435b9f8b4d55b5e2c59c3e
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-03 06:22:47 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strspn - determine the length of a prefix substring
+  * @s: pointer to the string to be checked
+  * @accept: pointer to the acceptable characters
+  * Return: number of counts of accepted char
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counter;
+	int x;
+	int y;
+
+	counter = 0;
+
+	for (x = 0 ; s[x] ; x++)
+	{
+		for (y = 0 ; accept[y]; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				counert++;
+				break;
+			}
+		}
+		if (!accept[y])
+			break;
+	}
+	return (counter);
+}
