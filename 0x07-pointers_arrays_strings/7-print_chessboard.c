42fa0ab87c0731f7a46a3b12651529621475a090
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-03 08:28:03 -0700

    task7-cf

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -15,11 +15,11 @@ void print_chessboard(char (*a)[8])
 	{
 		for (y = 0 ; y < 8 ; y++)
 		{
-			_putchar(a[x][y]);
-			_putchar(' ');
+			putchar(a[x][y]);
+			putchar(' ');
 		}
 
-		_putchar('\n');
+		putchar('\n');
 
 	}
 }
