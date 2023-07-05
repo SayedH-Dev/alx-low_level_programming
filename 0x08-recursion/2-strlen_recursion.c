fcba6a77060d62c6f140e9ab68792b016d0063b9
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-05 07:02:35 -0700

    task1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 
 /**
   * _strlen_recursion - function that return the length of a string
-  * @s: pointer to char
+  * @s: pointer to string
   * Return: 0 if terminator or 1+_strlen_recursion(s+1)
   */
 int _strlen_recursion(char *s)
