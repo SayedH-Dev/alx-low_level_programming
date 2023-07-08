e0cf162ab48e7641283bfdfd7d3788ce23887a67
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-08 14:08:18 -0700

    modified-code

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -9,5 +9,34 @@
   */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int x;
+	int y;
+	int z;
+
+	y = 0;
+	z = -1;
+	for (x = 0 ; s[x] != '\0'; x++)
+	{
+		if (s[x] == '-')
+			y = y * -1;
+
+		if (s[x] > 47 && s[x] < 58)
+		{
+			if (x < 0)
+			{
+				y = (y * 10) - (s[x] - '0');
+			}
+			else
+			{
+				z = (s[x] - '0') * -1;
+			}
+
+			if (s[x + 1] < 48 || s[x + 1] > 57)
+				break;
+		}
+	}
+	if (y < 0)
+		z = z * -1;
+
+	return (y);
 }
