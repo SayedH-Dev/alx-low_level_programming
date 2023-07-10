a0c09b6f6e0c84b66e2e3a3615dcf392849debd4
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-10 09:55:46 -0700

    2-str_concat.c-cf

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,18 +18,16 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	else
+
+	char *conc = malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (conc == NULL)
 	{
-		char *conc = malloc(strlen(s1) + strlen(s2) + 1);
-
-		if (conc == NULL)
-		{
-			return (NULL);
-		}
-
-		strcpy(conc, s1);
-		strcat(conc, s2);
-
-		return (conc);
+		return (NULL);
 	}
+
+	strcpy(conc, s1);
+	strcat(conc, s2);
+
+	return (conc);
 }
