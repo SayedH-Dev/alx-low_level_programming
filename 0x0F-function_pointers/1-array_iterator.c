f158ffca4eee6fbc1184a8bfbd914c2dd73f837f
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-19 06:35:26 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size == 0 || !action)
 		return;
 
 	for (x = 0 ; x < size ; x++)
