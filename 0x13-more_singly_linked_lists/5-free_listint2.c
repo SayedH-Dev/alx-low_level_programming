8f6beb2eff917ef16f78aca292a64d93660ca28e
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-31 04:27:28 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - function that frees the memory
+  * allocated for a linked list
+  * @head: pointer to a pointer to the linked list head
+  * Return: void
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *start, *nNode; /* next node */
+
+	for (start = *head ; start != NULL ; )
+	{
+		nNode = start->next;
+
+		free(start);
+		start = nNode;
+	}
+
+	*head = NULL;
+}
+
