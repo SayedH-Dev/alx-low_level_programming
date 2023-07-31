3eb6371c6dbb2ca0e94777eb35be6128be3a68ec
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-07-31 11:45:21 -0700

    task0-cf

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,16 +12,47 @@ size_t print_listint(const listint_t *h)
 
 	nodeNum = 0;
 
-	if (h == NULL)
-		return;
-	else
+	if (h == 0)
+		return (0);
+
+	for (start = h ; start != NULL ; start = start->next)
 	{
-		for (start = h ; start != NULL ; start = start->next)
-		{
-			printf("%d\n", start->n);
-			nodeNum++;
-		}
+		printf("%d\n", start->n);
+		nodeNum++;
 	}
 	return (nodeNum);
 }
 
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {8, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
+}
