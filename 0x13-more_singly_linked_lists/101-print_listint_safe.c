3791912669474019b6c7a75d28838d7194c1713e
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-08-01 11:32:58 -0700

    trial101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,6 @@
 #include "lists.h"
  
 /**
-  * add_nodeint - function that adds a new node at the beginning of a linked
-  * list
-  * @head: pointer to a pointer to the head of linked list
-  * @n: integer to be added
-  * Return: address of the new element, or NULL if it failed
-  */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *newNode;
-
-	newNode = (listint_t *)malloc(sizeof(listint_t));
-
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = n;
-	newNode->next = (*head);
-	(*head) = newNode;
-
-	return (newNode);
-}
-
-#include "lists.h"
-
-/**
   * free_listint - function that frees the memory
   * allocated for a linked list
   * @head: pointer to the linked list head
