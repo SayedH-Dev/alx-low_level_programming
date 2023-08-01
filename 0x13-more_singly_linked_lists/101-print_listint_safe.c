1cbd294b05afa663e721f5027514713d429cda50
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-08-01 11:11:02 -0700

    freelist101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,26 @@
 #include "lists.h"
 
+#include "lists.h"
+
+/**
+  * free_listint - function that frees the memory
+  * allocated for a linked list
+  * @head: pointer to the linked list head
+  * Return: void
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *start, *temp;
+
+	for (start = head ; start != NULL ; )
+	{
+		temp = start;
+		start = start->next;
+
+		free(temp);
+	}
+}
+
 /**
   * print_listint_safe - function that prints linked list
   * by going through it once
