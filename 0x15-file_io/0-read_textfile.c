58aed53cccf8a9bc55be14c7b3b4786249ad21a8
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-08-07 15:27:55 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,38 +15,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	file = fopen(filename, "r");
-
 	if (file == NULL)
 		return (0);
-
 	tmp = (char *)malloc(letters);
-
 	if (tmp == NULL)
 	{
 		fclose(file);
 		return (0);
 	}
-
 	bytesr = fread(tmp, sizeof(char), letters, file);
-
 	if (bytesr <= 0)
 	{
 		free(tmp);
 		fclose(file);
 		return (0);
 	}
-
 	bytesw = write(STDOUT_FILENO, tmp, (size_t)bytesr);
-
 	if ((size_t)bytesw != (size_t)bytesr || bytesw <= 0)
 	{
 		free(tmp);
 		fclose(file);
 		return (0);
 	}
-
 	free(tmp);
 	fclose(file);
 	return (bytesr);
