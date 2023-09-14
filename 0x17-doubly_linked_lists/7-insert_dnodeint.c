c20184525f0be419f479e433188105d0b5e7ffd9
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-14 12:12:12 -0700

    task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,14 +5,17 @@
   * inserts a node at a given poition
   * @h: pointer to pointer to the list head
   * @idx: index where we have to add the new node
+  * @n: new node data
   * Return: address of the node or NULL if failed
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node, *node;
+	dlistint_t *current_node, *prev, *node;
 	unsigned int counter;
 
 	counter = 0;
+	current_node = *h;
+	prev = NULL;
 
 	if (h == NULL)
 		return (NULL);
@@ -20,24 +23,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	if (counter == idx && current_node == NULL)
-		return (add_dnodeint_end(h, n));
-
-	if (counter != idx)
-		return (NULL);
-
-	current_node = *h;
-	prev = NULL;
-	while(counter < idx && current_node != NULL)
+	while (counter < idx && current_node != NULL)
 	{
 		prev = current_node;
 		current_node = current_node->next;
 		counter++;
 	}
 
+	if (counter != idx)
+		return (NULL);
+
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
+	{
+		free_dlistint(*h);
 		return (NULL);
+	}
 	node->n = n;
 	node->prev = prev;
 	node->next = current_node;
