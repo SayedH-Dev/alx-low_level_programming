501713df1fe0daa51a72716d78f71b666f15b948
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-18 04:51:04 -0700

    bugfix

--- 0x18-dynamic_libraries/3-calc.h
+++ 0x18-dynamic_libraries/3-calc.h
@@ -1,27 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 #endif
--- 0x18-dynamic_libraries/3-op_functions.c
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -1,56 +1,57 @@
 #include "3-calc.h"
+#include <stdio.h>
 
 /**
-  * op_add - function that calculate the sum of two integers
+  * add - function that calculate the sum of two integers
   * @a: integer 1
   * @b: integer 2
   * Return: sum of a and b
   */
-int op_add(int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-  * op_sub - function that subtract two integers
+  * sub - function that subtract two integers
   * @a: integer 1
   * @b: integer 2
   * Return: subtract b from a
   */
-int op_sub(int a, int b)
+int sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-  * op_mul - function that multiplies two integers
+  * mul - function that multiplies two integers
   * @a: integer 1
   * @b: integer 2
   * Return: multi result
   */
-int op_mul(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-  * op_div - function that divide two integers
+  * div - function that divide two integers
   * @a: integer 1
   * @b: integer 2
  * Return: dividing result
  */
-int op_div(int a, int b)
+int div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
-  * op_mod - function that calculate the modulus of two integers
+  * mod - function that calculate the modulus of two integers
   * @a: integer 1
   * @b: integer 2
   * Return: modulus result
   */
-int op_mod(int a, int b)
+int mod(int a, int b)
 {
 	return (a % b);
 }
