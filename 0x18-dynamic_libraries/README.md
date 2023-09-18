cd11d70908b6226ab8b108fc357e6759ce39f992
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-09-18 03:08:57 -0700

    construction

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+Readme file for project 0x18. C - Dynamic libraries at ALX SE
---
+++ 0x18-dynamic_libraries/putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _putchar - function that prints character
+  * @c: character to be printed
+  * Return: 0
+  */
+int _putchar(char c)
+{
+	return (0);
+}
