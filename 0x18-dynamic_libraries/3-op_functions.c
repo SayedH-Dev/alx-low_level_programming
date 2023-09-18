8b1e5269093a6b1061c74b3901fbe88649d7f8e5
Elsayed Hassan <133334414+SayedH-Dev@users.noreply.github.com>
2023-09-18 13:09:59 +0200

    calc files upload

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+  * op_add - function that calculate the sum of two integers
+  * @a: integer 1
+  * @b: integer 2
+  * Return: sum of a and b
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - function that subtract two integers
+  * @a: integer 1
+  * @b: integer 2
+  * Return: subtract b from a
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - function that multiplies two integers
+  * @a: integer 1
+  * @b: integer 2
+  * Return: multi result
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - function that divide two integers
+  * @a: integer 1
+  * @b: integer 2
+ * Return: dividing result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - function that calculate the modulus of two integers
+  * @a: integer 1
+  * @b: integer 2
+  * Return: modulus result
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
